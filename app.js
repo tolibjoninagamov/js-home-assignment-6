424f04d71f722a99fd0dee4dcfa6c8348d719c88
Tolibjon <carlostony7.9.7@gmail.com>
2022-01-07 22:41:18 +0500

    her

---
+++ app.js
@@ -0,0 +1,55 @@
+// ex__1
+
+// let = +prompt("a");
+// let = +prompt("b");
+// function shokir(a, b) {
+//   if (a < b) {
+//     return a;
+//   } else {
+//     return b;
+//   }
+// }
+
+
+
+
+
+
+//ex__2
+// function her(x, n) {
+//   var result = x
+
+//   for (var i = 1; i < n; i++) {
+//     result *= x;
+//   }
+
+//   return result
+// }
+
+// let x = prompt("x?", '')
+// let n = prompt("n?", '')
+
+// alert(her(x, n))
+
+
+//ex__3
+// let n = +prompt("n", '');
+
+// for (i = 1; i <= n*2; i++) {
+  
+//   if (i % 2 != 0)
+//      alert(i);
+// }
+
+
+//ex 4
+// let x = Infinity;
+
+// shokirQale:
+//   for (let i = 2; i <= x; i++) {
+//     for (let j = 2; j < i; j++) {
+//       if (i % j == 0) continue shokirQale;
+//     }
+//     confirm(i);
+//   }
+
